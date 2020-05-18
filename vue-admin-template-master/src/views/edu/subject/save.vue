4beaf141e7cd9804d048e3e8ac843d92cb80ae90
Sher6J <liujx17614417627@outlook.com>
2020-05-18 15:11:49 +0800

    update

---
+++ vue-admin-template-master/src/views/edu/subject/save.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="app-container">
+    <el-form label-width="120px">
+      <el-form-item label="信息描述">
+        <el-tag type="info">excel模版说明</el-tag>
+        <el-tag>
+          <i class="el-icon-download"/>
+          <a :href="'/static/1.xlsx'">点击下载模版</a>
+        </el-tag>
+
+      </el-form-item>
+
+      <el-form-item label="选择Excel">
+        <el-upload
+            ref="upload" 
+            :auto-upload="false"
+            :on-success="fileUploadSuccess"
+            :on-error="fileUploadError"
+            :disabled="importBtnDisabled"
+            :limit="1"
+            :action="BASE_API+'/eduservice/edu-subject/addSubject'"
+            name="file"
+            accept="application/vnd.ms-excel">
+            <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+            <el-button
+                :loading="loading"
+                style="margin-left: 10px;"
+                size="small"
+                type="success"
+                @click="submitUpload">{{ fileUploadBtnText }}
+            </el-button>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+    data () {
+        return {
+            BASE_API: process.env.BASE_API, // 接口API地址
+            fileUploadBtnText: '上传到服务器', // 按钮文字
+            importBtnDisabled: false, // 按钮是否禁用,
+            loading: false
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        /**
+         * 点击按钮上传文件到接口里面，不是axis请求，只是表单提交
+         */
+        submitUpload() {
+            this.importBtnDisabled = true
+            this.loading = true
+            //js原生写法：document.getElementById("upload").submit()
+            this.$refs.upload.submit() //uploade即上面的ref="upload"，类似于id 
+        },
+        /**
+         * 上传成功
+         */
+        fileUploadSuccess() {
+            //提示信息
+            this.loading = false
+            this.$message({
+                message: '添加课程分类成功',
+                type: 'success'
+            })
+            //跳转课程分类列表页面
+        },
+        /**
+         * 上传失败
+         */
+        fileUploadError() {
+            //提示信息
+            this.loading = false
+            this.$message({
+                message: '导入课程分类失败',
+                type: 'error'
+            })
+        }
+    },
+}
+</script>
